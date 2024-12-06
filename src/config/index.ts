d22b433a111e3eca13d8b93f99617d81e7c9cad5
Rahad-Ullah <myemail.abc20@gmail.com>
2024-12-06 11:38:38 +0600

    config file setup

---
+++ src/config/index.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  jwt: {
+    jwt_secret: process.env.JWT_SECRET,
+    expires_in: process.env.EXPIRES_IN,
+    refresh_secret: process.env.REFRESH_SECRET,
+    refresh_expires_in: process.env.REFRESH_EXPIRES_IN,
+    reset_pass_secret: process.env.RESET_PASS_SECRET,
+    reset_pass_expires_in: process.env.RESET_PASS_EXPIRES_IN,
+  },
+  reset_pass_link: process.env.RESET_PASS_LINK,
+  emailSender: {
+    email: process.env.EMAIL,
+    app_pass: process.env.APP_PASS,
+  },
+  cloudinary: {
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import { Server } from "http";
 import app from "./app";
-// import config from "./config";
+import config from "./config";
 
 async function main() {
-  const server: Server = app.listen(5000, () => {
-    console.log(`Server is listening on port ${5000}`);
+  const server: Server = app.listen(config.port, () => {
+    console.log(`Server is listening on port ${config.port}`);
   });
 }
 
