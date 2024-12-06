f70258fddbaae86afd9e49843f2e28489b7ff503
Rahad-Ullah <myemail.abc20@gmail.com>
2024-12-06 15:43:43 +0600

    Auth gaurd middleware implemented

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { UserControllers } from './user.controller';
 import validateRequest from "../../middlewares/validateRequest";
 import { UserValidations } from "./user.validation";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
 router.post(
   "/create-admin",
+  auth(UserRole.ADMIN),
   validateRequest(UserValidations.createAdmin),
   UserControllers.createAdmin
 );
